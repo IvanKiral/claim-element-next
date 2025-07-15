9c97b609cf9769909fef2c5abd1859afe6f2f068
Ivan Kiral <kiralivan@gmail.com>
2025-07-15 10:50:57 +0200

    add listing of items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,34 @@
+"use client";
+
+import type { ArticleType } from "@/model";
+import { useEffect, useState } from "react";
+import { ContentItemList } from "./components/ContentItemList";
 
 
 export default function Home() {
+  const [items, setItems] = useState<ReadonlyArray<ArticleType>>([]);
+
+  useEffect(() => {
+    fetch("/api/listItems")
+      .then((res) => res.json())
+      .then((data) =>
+        setItems(data as ReadonlyArray<ArticleType>));
+  }, []);
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-4xl font-bold">Hello World</h1>
+    <div className="container mx-auto px-4 py-8">
+      <header className="mb-8">
+        <h1 className="text-4xl font-bold text-blue-600 mb-2">Kontent.ai Items Queue MVP</h1>
+        <p className="text-gray-600">Manage and assign unassigned content items</p>
+      </header>
+      <main className="bg-white rounded-lg shadow-lg p-6">
+        <ContentItemList
+          items={items.map((item) => ({
+            id: item.system.id,
+            name: item.elements.title.value,
+          }))}
+          onAssignItem={() => { }}
+        />
       </main>
     </div>
   );
